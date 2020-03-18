038d07b5d40b9ac432af26fa7676928a6e2bc39b
nermineslimane <slimane.nermine@esprit.tn>
2020-03-18 17:52:04 +0100

    database setup

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
+//Connect to database
+connectDB();
 app.get('/', (req, res) => res.send('API running'));
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log('Server started on port ${PORT}'));
+app.listen(PORT, () => console.log('Server started on port {$PORT}'));
