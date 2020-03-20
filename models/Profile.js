a773241ab9e842cfa11cf405da7213b69120d3b5
nermineslimane <slimane.nermine@esprit.tn>
2020-03-20 20:49:28 +0100

    commit

--- models/Profile.js
+++ models/Profile.js
@@ -27,7 +27,7 @@ const ProfileSchema = new mongoose.Schema({
   githubusername: {
     type: String
   },
-  experiences: [
+  experience: [
     {
       title: {
         type: String,
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -110,4 +110,97 @@ router.get('/', async (req, res) => {
   }
 })
 
+//@route        GET api/profile/user/:user_is
+//@desc         GET profile by userID
+//@access       Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate('user', ['name', 'avatar'])
+    if (!profile)
+      return res.status(400).json({ msg: 'There is no profile for this user' })
+    res.json(profile)
+  } catch (err) {
+    console.error.message
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'There is no profile for this user' })
+    }
+    res.status(500).send('Server Error ..')
+  }
+})
+
+//@route        DELETE api/profile
+//@desc         DELETE profile , user  posts
+//@access       Public
+router.delete('/', auth, async (req, res) => {
+  try {
+    //Remove user posts
+
+    //remove profile
+    await Profile.findOneAndRemove({ user: req.user.id })
+    //remove User
+    await User.findOneAndRemove({ _id: req.user.id })
+
+    res.json({ msg: 'User deleted' })
+  } catch (err) {
+    console.error.message
+    res.status(500).send('Server Error ..')
+  }
+})
+//@route        PUT api/profile/experience
+//@desc         ADD experience to profile
+//@access       Private
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Title is required')
+        .not()
+        .isEmpty(),
+
+      check('company', 'Company is required')
+        .not()
+        .isEmpty(),
+
+      check('from', 'From date is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    }
+    try {
+      const profile = await Profile.findOne({ user: req.user.id })
+      profile.experience.unshift(newExp)
+      await profile.save()
+      res.json(profile)
+    } catch (err) {
+      console.error(err.message)
+      res.status(500).send('Server Error')
+    }
+  }
+)
 module.exports = router
