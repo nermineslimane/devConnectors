45dc9d400cfc87aadec79ace602ed7422726f600
nermineslimane <slimane.nermine@esprit.tn>
2020-03-26 20:53:21 +0100

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,24 @@ import Landing from './components/layout/Landing'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Register from './components/auth/Register'
 import Login from './components/auth/Login'
-
+//REDUX STUFF
+import { Provider } from 'react-redux'
+import store from './store'
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar></Navbar>
-      <Route exact path='/' component={Landing}></Route>
-      <section className='container'>
-        <Switch>
-          <Route exact path='/register' component={Register}></Route>
-          <Route exact path='/login' component={Login}></Route>
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar></Navbar>
+        <Route exact path='/' component={Landing}></Route>
+        <section className='container'>
+          <Switch>
+            <Route exact path='/register' component={Register}></Route>
+            <Route exact path='/login' component={Login}></Route>
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 )
 
 export default App
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,51 @@
-import React from 'react'
-
+import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom'
 const Login = () => {
-  return <div>Login</div>
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+  const { email, password } = formData
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  const onSubmit = e => {
+    e.preventDefault()
+    console.log('SUCCESS')
+  }
+  return (
+    <Fragment>
+      <div class='alert alert-danger'>Invalid credentials</div>
+      <h1 class='large text-primary'>Sign In</h1>
+      <p class='lead'>
+        <i class='fas fa-user'></i> Sign into Your Account
+      </p>
+      <form class='form' onSubmit={e => onSubmit(e)}>
+        <div class='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            required
+            value={email}
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <div class='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={password}
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <input type='submit' class='btn btn-primary' value='Login' />
+      </form>
+      <p class='my-1'>
+        Don't have an account? <Link to='/Register'>Sign Up</Link>
+      </p>
+    </Fragment>
+  )
 }
 
 export default Login
